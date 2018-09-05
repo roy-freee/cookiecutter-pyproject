b92ef2f9df29e034e89fe627bf7b57fcf2ae8146
Hiroyuki Tanaka <h.tanaka@freee.co.jp>
2018-09-05 11:21:19 +0900

    Fix: version import

--- {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/__init__.py
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/__init__.py
@@ -1,7 +1,7 @@
 {%- if cookiecutter.support_python2 != "n" %}
 # coding: utf-8
 from __future__ import absolute_import, division, print_function, unicode_literals
+{% endif %}
 from .__version__ import __version__
 
-{% endif %}
 __all__ = ['__version__']
